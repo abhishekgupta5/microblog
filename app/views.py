52d8307da28fd5f232297392d402a22e559bc7a9
abhishekgupta5 <abhishekmodi2011@gmail.com>
2016-05-21 02:45:15 +0530

    Added base template & fake posts: part-2

--- app/views.py
+++ app/views.py
@@ -1,7 +1,27 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "This is flask"
+    user = {'nickname':'Abhishek'}
+    posts = [
+            {
+	        'author': {'nickname':'Nippo'},
+		'body': 'Best goddamn roller in town!'
+	    },
+	    {
+ 	        'author': {'nickname':'Mamma'},
+		'body': 'Ahhhhh!'
+	    },
+	    {
+ 	        'author': {'nickname':'Akhil'},
+		'body': 'I am a great farter!'
+	    },
+	    {
+ 	        'author': {'nickname':'Yash'},
+		'body': 'I am the most logical man ever!'
+	    }	    	    	    
+              ]
+    return render_template('index.html',title='Home',user=user,posts=posts)
 
