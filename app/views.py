43b6da7408a13af4a71148933ed023086442aca6
abhishekgupta5 <abhishekmodi2011@gmail.com>
2016-05-26 18:17:38 +0530

    OpenIDs added- Part-3

--- app/views.py
+++ app/views.py
@@ -32,4 +32,4 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for OpenID="%s", remember_me="%s"' %(form.openid.data, str(form.remember_me.data)))
         return redirect('/index')
-    return render_template('login.html',title='Sign In',form=form)
+    return render_template('login.html', title='Sign In', form=form, providers=app.config['OPENID_PROVIDERS'])
