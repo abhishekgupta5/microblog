57383ad63e609289734fcc3d2a3a2d67fd44b1ee
abhishekgupta5 <abhishekmodi2011@gmail.com>
2016-05-29 02:45:02 +0530

    Profile page Part-6

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from hashlib import md5
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -26,6 +27,9 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
+
+    def avatar(self, size):
+        return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' % (md5(self.email.encode('utf-8')).hexdigest(), size)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
--- app/views.py
+++ app/views.py
@@ -75,3 +75,15 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user == None:
+        flash('User %s not found.' % nickname)
+        return redirect(url_for('index'))
+    posts = [
+            {'author': user, 'body': 'Test post #1'},
+            {'author': user, 'body': 'Test post #2'}
+            ]
+    return render_template('user.html',user=user, posts=posts)
