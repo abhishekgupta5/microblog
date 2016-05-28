0549c84b94e98300891b9f1e6b5c260af1464cca
abhishekgupta5 <abhishekmodi2011@gmail.com>
2016-05-28 19:57:57 +0530

    Functional user login system implemented Part-5

--- app/models.py
+++ app/models.py
@@ -5,6 +5,24 @@ class User(db.Model):
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)
+        except NameError:
+            return str(self.id)
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
