b9663a8f6514e5c09997cca16817b6ee50298232
abhishekgupta5 <abhishekmodi2011@gmail.com>
2016-07-14 05:46:01 +0530

    Dates and Times as per user location, Part-13

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
 from flask.ext.mail import Mail
 from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
+from .momentjs import momentjs
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -14,6 +15,7 @@ lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
+app.jinja_env.globals['momentjs'] = momentjs
 
 from app import views, models
 
